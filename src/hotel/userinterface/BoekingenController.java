cae5cd5c8f1ca2917e8fb92aa079f00928c232bb
huseyinucar <huseyin.ucar@student.hu.nl>
2020-06-16 17:08:07 +0200

    Boekingen.fxml aangemaakt
    HotelOverzichtController:
    toonBoekingen() methode uitgebreidt.
    nieuwBoeking() methode uitgebreidt.
    
    BoekingenController:
    geschreven initialize methode.
    reset methode knop uitgewerkt.
    methode voor boek knop uitgewerkt.
    Try-Exception foutmeldingen label weergeven.

---
+++ src/hotel/userinterface/BoekingenController.java
@@ -0,0 +1,63 @@
+package hotel.userinterface;
+
+import hotel.model.*;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class BoekingenController {
+    @FXML private TextField naamTextField;
+    @FXML private TextField adresTextField;
+    @FXML private DatePicker aankomstdatumDatePicker;
+    @FXML private DatePicker vertrekdatumDatePicker;
+    @FXML private ComboBox<KamerType> kamertypeComboBox;
+    @FXML private Button buttonBoek;
+    @FXML private Label labelGegevens;
+
+    private Hotel hotel = Hotel.getHotel();
+
+    public void initialize() {
+        labelGegevens.setText("");
+        List<KamerType> kamertypes = hotel.getKamerTypen();
+        kamertypeComboBox.setItems(FXCollections.observableList(kamertypes));
+    }
+
+    public void boek() {
+        LocalDate checkDatum = LocalDate.now().minusDays(1);
+        LocalDate aankomst = aankomstdatumDatePicker.getValue();
+        LocalDate vertrek = vertrekdatumDatePicker.getValue();
+
+        boolean nietInVerleden = aankomst.isAfter(checkDatum) && vertrek.isAfter(checkDatum);
+        boolean vertrekNaAankomst = vertrek.isAfter(aankomst);
+        boolean textIngevuld = naamTextField.getText() != null && adresTextField != null;
+        boolean kamerSelected = !kamertypeComboBox.getSelectionModel().isEmpty();
+
+        try {
+            if (textIngevuld && nietInVerleden && vertrekNaAankomst && kamerSelected) {
+                Hotel.getHotel().voegBoekingToe(aankomst, vertrek, naamTextField.getText(),
+                        adresTextField.getText(), kamertypeComboBox.getValue());
+                labelGegevens.setText("Succesvolle boeking!");
+                Stage stage = (Stage) buttonBoek.getScene().getWindow();
+                stage.close();
+            }
+            else{
+                labelGegevens.setText("De ingevoerde gegevens zijn niet correct!");
+            }
+        }
+        catch (Exception e) {
+            labelGegevens.setText("Kamers zijn niet beschikbaar!");
+        }
+    }
+
+    public void reset() {
+        naamTextField.setText(null);
+        adresTextField.setText(null);
+        aankomstdatumDatePicker.setValue(null);
+        vertrekdatumDatePicker.setValue(null);
+        kamertypeComboBox.setValue(null);
+    }
+}
--- src/hotel/userinterface/HotelOverzichtController.java
+++ src/hotel/userinterface/HotelOverzichtController.java
@@ -1,19 +1,25 @@
 package hotel.userinterface;
 
+import hotel.model.Boeking;
 import hotel.model.Hotel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class HotelOverzichtController {
     @FXML private Label hotelnaamLabel;
-    @FXML private ListView boekingenListView;
+    @FXML private ListView<String> boekingenListView;
     @FXML private DatePicker overzichtDatePicker;
 
     private Hotel hotel = Hotel.getHotel();
@@ -24,32 +30,47 @@ public class HotelOverzichtController {
         toonBoekingen();
     }
 
-    public void toonVorigeDag(ActionEvent actionEvent) {
+    public void toonVorigeDag() {
         LocalDate dagEerder = overzichtDatePicker.getValue().minusDays(1);
         overzichtDatePicker.setValue(dagEerder);
     }
 
-    public void toonVolgendeDag(ActionEvent actionEvent) {
+    public void toonVolgendeDag() {
         LocalDate dagLater = overzichtDatePicker.getValue().plusDays(1);
         overzichtDatePicker.setValue(dagLater);
     }
 
-    public void nieuweBoeking(ActionEvent actionEvent) {
-        System.out.println("nieuweBoeking() is nog niet geïmplementeerd!");
+    public void nieuweBoeking() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Boekingen.fxml"));
+        Parent root = loader.load();
+        Stage newStage = new Stage();
+        newStage.setScene(new Scene(root));
+        newStage.initModality(Modality.APPLICATION_MODAL);
+        newStage.showAndWait();
+        initialize();
+    }
 
         // Maak in je project een nieuwe FXML-pagina om boekingen te kunnen invoeren
         // Open de nieuwe pagina in deze methode
         // Zorg dat de gebruiker ondertussen geen gebruik kan maken van de HotelOverzicht-pagina
         // Update na sluiten van de nieuwe pagina het boekingen-overzicht
-    }
 
     public void toonBoekingen() {
-        System.out.println("toonBoekingen() is nog niet geïmplementeerd!");
         ObservableList<String> boekingen = FXCollections.observableArrayList();
+        LocalDate datum = overzichtDatePicker.getValue();
+        Hotel hotel = Hotel.getHotel();
 
+        for (Boeking boeking : hotel.getBoekingen()) {
+            LocalDate aankomstdatum = boeking.getAankomstDatum();
+            LocalDate vertrekdatum = boeking.getVertrekDatum();
+            if ((aankomstdatum.isBefore(datum.plusDays(1)) && vertrekdatum.isAfter(datum.minusDays(1)))) {
+                boekingen.add(String.format("Begindatum: %tD,  Einddatum: %tD,  Kamernummer: %d,  Klantnaam: %s",
+                        aankomstdatum, vertrekdatum, boeking.getKamer().getKamerNummer(),
+                        boeking.getBoeker().getNaam()));
+            }
+        }
         // Vraag de boekingen op bij het Hotel-object.
         // Voeg voor elke boeking in nette tekst (string) toe aan de boekingen-lijst.
-
         boekingenListView.setItems(boekingen);
     }
 }
